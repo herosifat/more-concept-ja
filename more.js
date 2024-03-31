fe68b525b9263584939daaad0203e8870714dfc6
Your Namesifathero <sifathero466@gmail.com>
2024-03-31 19:25:59 +0600

    p

--- more.js
+++ more.js
@@ -1,5 +1,22 @@
-console.log(1)
-console.log(2)
-console.log(3)
-console.log(4)
+console.log(1);
+console.log(2);
+setTimeout(() =>{
+    console.log(3)
+},3000);
+console.log(4);
+console.log(5);
+console.log(6);
 
+let num=0;
+const clockId = setInterval(() => {
+    num++;
+    clearInterval(clockId)
+    console.log(clockId,num)
+}, 2000);
+
+/**
+ * * kono ekta kaj ke nidestho time por gotaile chaile "settimeout" use kora hoy.
+
+*kono ekta kaj ke nidestho time por bar bar gotaile chaile "setinterval" use kora hoy
+
+ */
